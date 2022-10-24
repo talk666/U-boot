ff2f51dff62d62c337b3892ee129d1952aef3b5c
talk666 <137696814@qq.com>
2022-10-24 09:37:51 -0700

    添加自己的板级文件并编译出来，涉及修改的所有文件--uboot移植1

--- uboot-imx-rel_imx_4.1.15_2.1.0_ga/mx6ull_14x14_emmc.sh
+++ uboot-imx-rel_imx_4.1.15_2.1.0_ga/mx6ull_14x14_emmc.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make mx6ull_14x14_evk_emmc_defconfig
+make mx6ull_14x14_glxadd_emmc_defconfig
 make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
+
+if [[ $1 -eq 1 ]];then
+    ./imxdownload u-boot.bin /dev/sdb1
+fi
 
